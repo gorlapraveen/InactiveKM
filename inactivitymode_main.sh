cb8e2cf01e4d74d2072b4f30bfb4489e51d0b175
gorlapraveen <gorlapraveen@gmail.com>
2018-10-03 23:24:33 +0700

    added inactivitymode_main.sh

---
+++ inactivitymode_main.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+usage()
+{
+echo 'Supported System Action (mode) : sleepmode, suspend, hibernate, shutdown. restart'
+echo ' '
+echo 'Suppoted System Action in (time) in minutes : numerical' 
+echo ' '
+echo 'Supported System Action in (Battery) Level : 1 to 100 values'
+echo ''
+echo 'Example Usage: sleep 60 30  # System "sleeps" after 60 minutes or when below 30 % of battery level, or which ever comes earlier when Keyboard/mouse is inactivite'
+
+}
+
+#usage
+
+mode=$1
+time_period=$2
+Battery_level=$3
+echo "$mode" "$time_period" "$Battery_level"
+#1 minute = 1000 Milliseconds
+#]sleepmode=sleepmode
+#hibernatemode=hibernatemode
+#suspendmode=suspendmode
+#shutdown=shutdownmode
+#restartmode=restartmode
+#echo $restartmode $sleepmode
+
+if [[ ! $time_period =~ ^[[:digit:]]+$ ]]; then
+ echo Hey $USER, please input only an integer 
+ usage
+elif [[ "$mode" != 'sleepmode' ]] || [[ "$mode" != 'hibernatemode' ]] || [[ "$mode" != 'suspendmode' ]] || [[ "$mode" != '$shutdownmode' ]] || [[ "$mode" != '$restartmode' ]]; then
+ echo Hey $USER, please use appropriate mode
+ usage
+elif [[ ! $Battery_level =~ ^[[:digit:]]+$ ]]; then
+ echo $USER, please input an integer
+ usage
+else
+ ./inactivitymode.sh 
+fi
+    
+
+
+
