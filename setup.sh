f68f18427d4d94437f5191f93b8e48061b7ca83c
gorlapraveen <gorlapraveen@gmail.com>
2018-10-19 04:48:24 +0700

    setup.sh add

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-sudo chmod +x backlight.sh
-sudo cp backlight.sh /usr/local/bin/
-sudo cp backlight.sh /usr/bin/backlight
-sudo chmod +x /usr/bin/backlight
+sudo chmod +x inactivity.sh
+sudo cp inactivity.sh /usr/local/bin/
+sudo cp inactivity.sh /usr/bin/inactivity
+sudo chmod +x /usr/bin/inactivity
 
+sudo chmod +x inactivitymode_main.sh 
+sudo cp inactivitymode_main.sh  /usr/local/bin/
+sudo cp inactivitymode_main.sh  /usr/bin/inactivitymode_main.sh 
+sudo chmod +x /usr/bin/inactivitymode_main.sh 
 
+sudo chmod +x  inactivity.txt 
+sudo cp inactivity.txt  /usr/local/bin/
+sudo cp inactivity.txt  /usr/bin/inactivity.txt 
+sudo chmod +x /usr/bin/inactivity.txt 
