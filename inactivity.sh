8efd216f46be49935d7387efc9b6925e949eed60
gorlapraveen <gorlapraveen@gmail.com>
2018-10-19 04:54:05 +0700

    setup.sh add

--- inactivity.sh
+++ inactivity.sh
@@ -1,123 +1,91 @@
 #!/bin/bash
-#echo 'Script is initiated for your Inactive mode'
-#echo '========'
-#echo "$1" "$2" "$3"
-#echo '========'
-IdleTimeMillsec=$(xprintidle)
-echo 'Idle for '"$IdleTimeMillsec"
-BatteryStatus=$(acpi)
-echo 'Battery Status '"$BatteryStatus"
-BatteryArray=($BatteryStatus)
-#echo ${BatteryArray[3]}
-BatteryArrayPer=$(echo ${BatteryArray[3]})
-#echo "$BatteryArrayPer"
-BatteryArrayPerLen=${#BatteryArrayPer}
-#echo "$BatteryArrayPerLen"
-if [ $BatteryArrayPerLen -eq 5 ]; then
- BatteryArrayPerNum=${BatteryArrayPer:0:3}
-elif [ $BatteryArrayPerLen -eq 4 ]; then
-   BatteryArrayPerNum=${BatteryArrayPer:0:2}
-elif [ $BatteryArrayPerLen -eq 3 ]; then
-   BatteryArrayPerNum=${BatteryArrayPer:0:1}
-elif [ $BatteryArrayPerLen -lt 2 ]; then
-   echo "Unknown Battery Percentage"
-fi
-echo "$BatteryArrayPerNum"
 
-readarray InactivityArray < inactivity.txt
-LenInactiveArray=${#InactivityArray[@]}
-BatteryOut=${InactivityArray[$LenInactiveArray-1]}
-TimeOut=${InactivityArray[$LenInactiveArray-2]}
-TimeOutMilli=$((($TimeOut)*6000))
-echo "$TimeOutMilli"
-ModeOut=${InactivityArray[$LenInactiveArray-3]}
+usage()
+{
+echo 'Supported System Action (mode) : haltmode, poweroffmode, rebootmode, suspendmode, hibernatemode, hybrid-sleepmode'
+echo ' '
+echo 'Suppoted System Action in (time) in minutes : numerical' 
+echo ' '
+echo 'Supported System Action in (Battery) Level : 1 to 100 values'
+echo ''
+echo 'Example Usage: sleep 60 30  # System "sleeps" after 60 minutes or when below 30 % of battery level, or which ever comes earlier when Keyboard/mouse is inactivite'
 
-#Countdown timer, which can be used to give advance alaram, prior to  when certan operation is going to happen.
-countdown()
-(
-  IFS=:
-  set -- $*
-  secs=$(( ${1#0} * 3600 + ${2#0} * 60 + ${3#0} ))
-  while [ $secs -gt 0 ]
-  do
-    sleep 1 &
-    printf "\r%02d:%02d:%02d" $((secs/3600)) $(( (secs/60)%60)) $((secs%60))
-    secs=$(( $secs - 1 ))
-    wait
-  done
-  echo
-)
+}
 
-if [[ $BatteryArrayPerNum -le $BatteryOut ]]; then
- case "$ModeOut" in
-  haltmode )
-   echo 'Your system will halt in'
-   countdown "00:00:30" # 30 sec 
-   bash -c "sudo systemctl halt"
-   ;;
-  poweroffmoode )
-   echo 'Your system will poweroff in'
-   countdown "00:00:30" # 30 sec;
-   bash -c " sudo systemctl poweroff"
-   ;;
-  rebootmode )
-   echo 'Your system will reboot in'
-   countdown "00:00:30" # 30 sec
-   bash -c "sudo systemctl reboot "
-   ;;
-  suspendmode )
-   echo 'Your system will suspend in'
-   countdown "00:00:30" ; # 30 sec
-   bash -c "sudo systemctl suspendmode"
-   ;;
-  hibernatemode )
-   echo 'Your system will hybernate in' 
-   countdown "00:00:30" ; # 30 sec 
-   bash -c "sudo systemctl hibernate"
-   ;;
-  hybrid-sleepmode )
-   echo 'Your system will hybrid-sleepmode in' 
-   countdown "00:00:30" ; # 30 sec
-   bash -c "sudo systemctl hybrid-sleep"
-   ;;
-  *) echo 'Mode:' "$ModeOut" ' - is not a correct option'
-   ;;
- esac
-elif [[ $IdleTimeMillsec -gt $TimeOutMilli ]]; then
- case "$ModeOut" in
-  haltmode )
-   echo 'Your system will halt in' 
-   countdown "00:00:30" # 30 sec 
-   bash -c "sudo systemctl halt"
-   ;;
-  poweroffmoode )
-   echo 'Your system will poweroff in' 
-   countdown "00:00:30" # 30 sec
-   bash -c " sudo systemctl poweroff"
-   ;;
-  rebootmode )
-   echo 'Your system will reboot in' 
-   countdown "00:00:30" # 30 sec
-   bash -c "sudo systemctl reboot "
-   ;;
-  suspendmode )
-   echo 'Your system will suspend in' 
-   countdown "00:00:30" # 30 sec
-   bash -c "sudo systemctl suspendmode"
-   ;;
-  hibernatemode )
-   echo 'Your system will hybernate in'
-   countdown "00:00:30" # 30 sec
-   bash -c "sudo systemctl hibernate"
-   ;;
-  hybrid-sleepmode )
-   echo 'Your system will hybrid-sleepmode in'
-   countdown "00:00:30" # 30 sec
-   bash -c "sudo systemctl hybrid-sleep"
-   ;;
-  *) echo 'Mode:' "$ModeOut" ' - is not a correct option'
-   ;;
- esac
+#usage
+
+mode=$1
+time_period=$2
+Battery_level=$3
+echo "$mode" "$time_period" "$Battery_level"
+#1 minute = 6000 Milliseconds
+#]sleepmode=sleepmode
+#hibernatemode=hibernatemode
+#suspendmode=suspendmode
+#shutdown=shutdownmode
+#restartmode=restartmode
+#echo $restartmode $sleepmode
+
+if [[ ! $time_period =~ ^[[:digit:]]+$ ]]; then
+ echo Hey $USER, "$2" is not an Integer value, please input only an integer value
+ usage
+
+#elif [[ "$mode" != 'sleepmode' ]] || [[ "$mode" != 'hibernatemode' ]] || [[ "$mode" != 'suspendmode' ]] || [[ "$mode" != '$shutdownmode' ]] || [[ "$mode" != '$restartmode' ]]; then 
+#echo Hey $USER, please use appropriate mode
+ #usage
+elif [[ ! $Battery_level =~ ^[[:digit:]]+$ ]]; then
+ echo $USER, "$3" is not an Integer value, please input only an integer value
+ usage
 else
- ./inactivitymode.sh
+ case "$1" in
+  haltmode )
+   echo > inactivity.txt
+   echo $1 >inactivity.txt
+   echo $2 >>inactivity.txt
+   echo $3 >>inactivity.txt
+   bash inactivitymode.sh 
+   ;;
+  poweroffmoode )
+   echo > inactivity.txt
+   echo $1 >inactivity.txt
+   echo $2 >>inactivity.txt
+   echo $3 >>inactivity.txt
+   bash inactivitymode.sh
+   ;;
+  rebootmode )
+   echo > inactivity.txt
+   echo $1 >inactivity.txt
+   echo $2 >>inactivity.txt
+   echo $3 >>inactivity.txt
+   bash inactivitymode.sh
+   ;;
+  suspendmode )
+   echo > inactivity.txt
+   echo $1 >inactivity.txt
+   echo $2 >>inactivity.txt
+   echo $3 >>inactivity.txt
+   bash inactivitymode.sh $1 $2 $3
+   ;;
+  hibernatemode )
+   echo > inactivity.txt
+   echo $1 >inactivity.txt
+   echo $2 >>inactivity.txt
+   echo $3 >>inactivity.txt
+   bash inactivitymode.sh $1 $2 $3
+   ;;
+  hybrid-sleepmode )
+   echo > inactivity.txt
+   echo $1 >inactivity.txt
+   echo $2 >>inactivity.txt
+   echo $3 >>inactivity.txt
+   bash inactivitymode.sh $1 $2 $3
+   ;;
+  *) echo 'Mode:' "$1" ' - is not a correct option'
+     usage
+   ;;
+ esac
 fi
+    
+
+
+
--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ sudo cp inactivity.sh /usr/bin/inactivity
 sudo chmod +x /usr/bin/inactivity
 
 sudo chmod +x inactivitymode_main.sh 
-sudo cp inactivitymode_main.sh  /usr/local/bin/
-sudo cp inactivitymode_main.sh  /usr/bin/inactivitymode_main.sh 
-sudo chmod +x /usr/bin/inactivitymode_main.sh 
+sudo cp inactivitymode.sh  /usr/local/bin/
+sudo cp inactivitymode.sh  /usr/bin/inactivitymode.sh 
+sudo chmod +x /usr/bin/inactivitymode.sh 
 
 sudo chmod +x  inactivity.txt 
 sudo cp inactivity.txt  /usr/local/bin/
